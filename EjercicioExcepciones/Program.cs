fa62f09f4735608291d36b6158db9abaa068c186
Kevin Chacon <kevin27chacon@gmail.com>
2024-08-09 14:40:30 -0600

    creacion de clase operaciones

--- EjercicioExcepciones/Program.cs
+++ EjercicioExcepciones/Program.cs
@@ -1,1 +1,11 @@
 ﻿
+int opcion;
+
+Console.WriteLine("---Calculadora---");
+Console.WriteLine("Bienvenido a nuestro servicio de calculadora");
+Console.WriteLine("Por favor escoga una opción: ");
+Console.WriteLine("1. Suma");
+Console.WriteLine("2. Resta");
+Console.WriteLine("3. Multiplicación");
+Console.WriteLine("4. División");
+Console.WriteLine("9. Salir");
