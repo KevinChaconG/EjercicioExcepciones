88a3e7d416932194cc6a993d75b1ca19037162ee
Kevin Chacon <kevin27chacon@gmail.com>
2024-08-09 14:35:13 -0600

    Add project files.

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+        double n1;
+        double n2;
+
+        public double Suma()
+        {
+            return n1 + n2;
+        }
+
+        public double Resta()
+        {
+            return n1-n2;
+        }
+
+        public double Division()
+        {
+            return n1/n2;
+        }
+
+        public double Multiplicacion()
+        {
+            return n1*n2;
+        }
+    }
+}
